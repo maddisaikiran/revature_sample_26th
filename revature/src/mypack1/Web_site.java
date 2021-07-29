fd0640c9e5657fdf1302658340026b2b3ab77074
maddi sai kiran reddy <maddi.saikiran12345@gmail.com>
2021-07-29 17:24:00 +0530

    Four_java_programs

---
+++ revature/src/mypack1/Web_site.java
@@ -0,0 +1,12 @@
+package mypack1;
+
+public class Web_site {
+
+	public static void main(String[] args) {
+		 String s = "https://en.wikipedia.org/wiki/Main_Page";
+			String arr[] = s.split("/");
+			System.out.println(arr[2]);
+
+	}
+
+}
